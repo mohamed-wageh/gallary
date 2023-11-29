697a790958de729b0153e5d7f6adb8b5e9e0a4f6
Mahmoud shabaan <115648678+MaShabaan@users.noreply.github.com>
2023-11-29 11:55:22 +0200

    saveHashPasswordinDataBase

--- BackEnd/routes/userRoutrs.js
+++ BackEnd/routes/userRoutrs.js
@@ -1,86 +1,90 @@
-const router = require("express").Router();
-const User = require("../models/userModel");
-const jwt = require("jsonwebtoken")
-const bycrpt = require("bcryptjs")
+const router = require('express').Router();
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bycrpt = require('bcryptjs');
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
-    const { name , email , password , role , profilePicture} = req.body
-    if(!name || !email || !password){
+    const { name, email, password, role, profilePicture } = req.body;
+    if (!name || !email || !password) {
       return res.status(400).send({
         success: false,
-        message: "All Name , Email and  Password are required",
+        message: 'All Name , Email and  Password are required'
       });
     }
     //check if users exists
-    const userExist= await User.findOne({email})
-    if (userExist){
+    const userExist = await User.findOne({ email });
+    if (userExist) {
       return res.status(400).send({
         success: false,
-        message: "User already existed",
+        message: 'User already existed'
       });
     }
     //hash passowrd
-    const salt = await bycrpt.genSalt(10)
-    const hashPassword =await bycrpt.hash(password , salt)
+    const salt = await bycrpt.genSalt(10);
+    const hashPassword = await bycrpt.hash(password, salt);
+    //save hashpassword in database
+    const hashPasswords = async () => {
+      await Promise.all(
+        User.map(async user => {
+          const hashedPassword = await bcrypt.hash(user.password, 10);
+          user.password = hashedPassword;
+        })
+      );
+    };
     //create user
     const user = await User.create({
       name,
       email,
-      password:hashPassword,
+      password: hashPassword,
       role,
       profilePicture
-    })
-     if (user){
+    });
+    if (user) {
       res.status(201).send({
         success: true,
-        message: "User created successfully",
+        message: 'User created successfully'
       });
-     }
-     else{
+    } else {
       return res.status(400).send({
         success: false,
-        message: " Invalid User",
+        message: ' Invalid User'
       });
-     }
-    
+    }
   } catch (error) {
     res.send({
       success: false,
-      message: error.message,
+      message: error.message
     });
   }
 });
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
-    const { name , email , password } = req.body
-    if(!email || !password){
+    const { name, email, password } = req.body;
+    if (!email || !password) {
       return res.status(400).send({
         success: false,
-        message: "Both Email and Password are required",
+        message: 'Both Email and Password are required'
       });
     }
     //check if users exists
-    const user= await User.findOne({email})
-    if (user && await bycrpt.compare(password , user.password)){
+    const user = await User.findOne({ email });
+    if (user && (await bycrpt.compare(password, user.password))) {
       return res.status(400).send({
         success: true,
-        message: "Logged In",
+        message: 'Logged In'
       });
-    }
-   
-     else{
+    } else {
       return res.status(400).send({
         success: false,
-        message: " Invalid User",
+        message: ' Invalid User'
       });
-     }
-    
+    }
   } catch (error) {
     res.send({
       success: false,
-      message: error.message,
+      message: error.message
     });
   }
 });
