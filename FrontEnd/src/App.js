62c096f5d61fd908043349e41b47534a156738cd
maryam-hussien <maryamhussien122@gmail.com>
2024-04-22 08:53:16 +0200

    home routes and file structure

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -6,14 +6,23 @@ import Users from "./dashboard/Users";
 import Orders from "./dashboard/Orders";
 import DashProducts from "./dashboard/DashProducts";
 import DashSettings from "./dashboard/DashSettings";
+import Home from "./Component/Home/Home";
+import Profile from "./Component/Profile/Profile";
+import EditProfile from "./Component/EditProfile/EditProfile"
+import Cart from "./Component/Cart/Cart";
+import Checkout from "./Component/Checkout/Checkout"
 
 const router = createBrowserRouter([
-  { path: "/", element: <DashHome /> },
+  { path: "/", element: <Home /> },
   { path: "/dashboard/home", element: <DashHome /> },
   { path: "/dashboard/users", element: <Users /> },
   { path: "/dashboard/orders", element: <Orders /> },
   { path: "/dashboard/product", element: <DashProducts /> },
   { path: "/dashboard/setting", element: <DashSettings /> },
+  {path:"/profile" , element: <Profile />},
+  {path:"/profile/edit" , element: <EditProfile />},
+  {path:"/cart" , element: <Cart />},
+  {path: "/cart/checkout" , element:<Checkout />},
 ]);
 
 function App() {
---
+++ FrontEnd/src/Component/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Cart.css"
+import Navbar from '../.././Navbar/Navbar';
+import Footer from "../.././Footer/Footer"
+const Cart = () => {
+  return (
+    <>
+    <Navbar />
+      Cart
+      <Footer />
+    </>
+  )
+}
+
+export default Cart
